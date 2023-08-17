4802648b37b3acd631212a6927720963c1676fc3
artreimus <arthurreimus1623@gmail.com>
2023-08-17 17:13:01 +0800

    solved reversal

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,17 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  // Solution #1
+  //   return str.split('').reverse().join('');
+  // Solution #2
+  //   let reversed = '';
+  //   for (let char of str) {
+  //     reversed = char + reversed;
+  //   }
+  //   return reversed;
+  //   Solution #3
+  return str.split('').reduce((rev, char) => char + rev, '');
+}
 
 module.exports = reverse;

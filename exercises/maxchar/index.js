295fcee5ce2b24a4395cb09f88930dd650b3f9cf
artreimus <arthurreimus1623@gmail.com>
2023-08-23 15:56:28 +0800

    solved maxchars

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,50 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  // Solution #1
+  //   const charMap = {};
+
+  //   let maxKey = '';
+  //   let maxValue = 0;
+
+  //   for (let char of str) {
+  //     if (charMap[char]) {
+  //       charMap[char]++;
+  //     } else {
+  //       charMap[char] = 1;
+  //     }
+  //   }
+
+  //   for (let char in charMap) {
+  //     if (charMap[char] > maxValue) {
+  //       maxValue = charMap[char];
+  //       maxKey = char;
+  //     }
+  //   }
+
+  return maxKey;
+
+  // Solution #2
+  const charMap = {};
+
+  let maxKey = '';
+  let maxValue = 0;
+
+  for (let char of str) {
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+
+    if (charMap[char] > maxValue) {
+      maxValue = charMap[char];
+      maxKey = char;
+    }
+  }
+
+  return maxKey;
+}
 
 module.exports = maxChar;

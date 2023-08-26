3d13166ad3e5a53a832874acfa429487d78ba577
artreimus <arthurreimus1623@gmail.com>
2023-08-26 16:08:44 +0800

    solved array chunking

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,55 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+// If length === size:
+// push chunk into chunks
+//
+
+function chunk(array, size) {
+  //   Solution #1
+  //   const chunks = [];
+  //   const chunk = [];
+  //   for (let i = 0; i < array.length; i++) {
+  //     if (chunk.length === size) {
+  //       chunks.push([...chunk]);
+  //       chunk.splice(0, chunk.length);
+  //     }
+  //     if (i === array.length - 1) {
+  //       chunks.push([...chunk, array[i]]);
+  //     } else {
+  //       chunk.push(array[i]);
+  //     }
+  //   }
+  //   return chunks;
+  //   Solution #2
+  //   const chunked = [];
+  //   for (let element of array) {
+  //     const last = chunked[chunked.length - 1];
+  //     if (!last || last.length === size) {
+  //       chunked.push([element]);
+  //     } else {
+  //       last.push(element);
+  //     }
+  //   }
+  //   return chunked;
+  // Solution #3
+  const chunked = [];
+  let index = 0;
+
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size));
+    index += size;
+  }
+
+  return chunked;
+
+  //   Solution #4
+  //   const chunked = [];
+
+  //   while (array.length > 0) {
+  //     chunked.push(array.splice(0, size));
+  //   }
+  //   return chunked;
+}
 
 module.exports = chunk;

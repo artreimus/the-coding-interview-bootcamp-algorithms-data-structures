19ef97d9e099fa346e511bc3e9d531673b4f244e
artreimus <arthurreimus1623@gmail.com>
2023-08-29 23:57:32 +0800

    solved anagarams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,46 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function generateCharMap(str, charMap) {
+  for (let char of str) {
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+  }
+}
+
+// Solution #1
+// function anagrams(stringA, stringB) {
+//   const charMapA = {};
+//   const charMapB = {};
+
+//   stringA = stringA.replace(/[^\w]/g, '').toLowerCase();
+//   stringB = stringB.replace(/[^\w]/g, '').toLowerCase();
+
+//   generateCharMap(stringA, charMapA);
+//   generateCharMap(stringB, charMapB);
+
+//   const keysA = Object.keys(charMapA);
+//   const keysB = Object.keys(charMapB);
+
+//   if (keysA.length !== keysB.length) return false;
+
+//   for (let key of keysA) {
+//     if (!charMapB[key] | (charMapA[key] !== charMapB[key])) return false;
+//   }
+
+//   return true;
+// }
+
+function cleanString(str) {
+  return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+}
+
+// Solution #2
+function anagrams(stringA, stringB) {
+  return cleanString(stringA) === cleanString(stringB);
+}
 
 module.exports = anagrams;

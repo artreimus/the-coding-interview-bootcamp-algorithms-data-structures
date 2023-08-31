bd5a245c425756106e78ab3af1d99a6b6ceb61af
artreimus <arthurreimus1623@gmail.com>
2023-09-01 02:57:47 +0800

    complete capitalize first letter

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,28 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+// Solution #1
+// function capitalize(str) {
+//   const wordsArr = str.split(' ');
+//   for (let i = 0; i < wordsArr.length; i++) {
+//     wordsArr[i] = wordsArr[i][0].toUpperCase() + wordsArr[i].slice(1);
+//   }
+
+//   return wordsArr.join(' ');
+// }
+
+// Solution #2
+function capitalize(str) {
+  const wordsArr = str.split('');
+
+  wordsArr[0] = wordsArr[0].toUpperCase();
+  for (let i = 2; i < wordsArr.length; i++) {
+    if (wordsArr[i - 1] === ' ') {
+      wordsArr[i] = wordsArr[i].toUpperCase();
+    }
+  }
+
+  return wordsArr.join('');
+}
 
 module.exports = capitalize;
